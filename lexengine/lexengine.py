fe5eacc2a9ce78dbe002067e274b6b1e00de211e
trevaris_small <trevaris.small@traxretail.com>
2020-05-02 02:04:09 -0400

    Add POST `languages` view

--- lexengine/lexengine.py
+++ lexengine/lexengine.py
@@ -1,8 +1,9 @@
+from collections import OrderedDict
 from flask import (
-    Blueprint, render_template
+    Blueprint, flash, redirect, render_template, request, url_for
 )
 
-from .data import get_db, select
+from .data import get_db, select, insert, get_languages
 
 bp = Blueprint('lexengine', __name__)
 
@@ -10,9 +11,33 @@ bp = Blueprint('lexengine', __name__)
 def index():
     return render_template("index.html")
 
-@bp.route('/languages/')
+@bp.route('/languages/', methods=('GET', 'POST'))
 def languages():
-    return render_template('languages.html', languages=select("languages", coerce=True))
+    if request.method == 'POST':
+        values = {key:val for key, val in request.form.items()}
+        values['ancestor_id'] = None
+
+        if ancestor_name := values['ancestor']:            
+            try:
+                ancestor = select("languages", name=ancestor_name, coerce=True)[0]
+            except IndexError:
+                insert("languages", values=[ancestor_name, ancestor_name, None, None, None, None])
+                ancestor = select("languages", name=ancestor_name, coerce=True)[0]
+            values['ancestor_id'] = ancestor.language_id
+
+        error = None        
+        if select("languages", name=values['name']):
+            error = "Language already exists in database."
+
+        if not error:
+            columns = ['name', 'eng_name', 'ancestor_id', 'iso_639_1', 'iso_639_2', 'iso_639_3']
+            insert("languages", values=[values[col] for col in columns])
+            return redirect(url_for('lexengine.languages'))
+
+        flash(error)
+
+    #return render_template('languages.html', languages=select("languages", coerce=True))
+    return render_template('languages.html', languages=get_languages())
 
 @bp.route('/<language>/lexicon/', methods=('GET', 'POST'))
 def lexicon(language):
